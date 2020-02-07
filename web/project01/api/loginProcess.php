639e5cc7402ef99e6b775beccb635ac17891bcb7
mcgaryp <portermcgary@me.com>
2020-02-07 15:54:28 -0700

    combined login and process

--- web/project01/api/loginProcess.php
+++ web/project01/api/loginProcess.php
@@ -1,9 +1,62 @@
 <?php
+
+   print("hello from process movies");
    session_start();
 
    $_SESSION["Account"];
    // https://www.youtube.com/watch?v=PXugYdXCBck
 
-   require_once "processMovies.php";
+   // Build some kind of object to pass back to the javascript
+
+   class Movie {
+      public $image;
+      public $title;
+      public $description;
+      public $rating;
+      public $year;
+
+      function Movie($image, $title, $description, $rating, $year) {
+         $this->image = $image;
+         $this->title = $title;
+         $this->description = $description;
+         $this->rating = $rating;
+         $this->year = $year;
+      }
+   }
+
+   // Get data from db
+   print("hello");
+   require_once "dbConnect.php";
+
+   // necessary to catch errors thrown from other files
+   function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+      throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+   }
+   set_error_handler("exception_error_handler");
+
+   $db = getBD();
+
+   // Just get some of the movies preparation
+   $movieDB = $db->prepare("SELECT * FROM movie m INNER JOIN movie_group mg on m.movie_id = mg.movie_id AND mg.account_id = 2");
+   $movieDB->execute();
+
+   // Create array to hold all the users movies
+   $movies = array();
+
+   // Pass it into the object
+   while($row = $moviesDB->fetch(PDO::FETCH_ASSOC)) {
+      $movie = new Movie($row["image"], $row["title"], $row["description"], $roe["rating"], $row["year"]);
+      array_push($movies, $movie);
+   }
+
+   // Set object in session
+   $_SESSION["Movies"] = $movies;
+
+   print_r($_SESSION);
+
+   // Redirect to home.php
+   header("Location: ../home.php");
+   exit();
+
 
 ?>
--- web/project01/api/processMovies.php
+++ web/project01/api/processMovies.php
@@ -1,53 +1,6 @@
 <?php
 
-// Build some kind of object to pass back to the javascript
-class Movie {
-   public $image;
-   public $title;
-   public $description;
-   public $rating;
-   public $year;
+session_start();
 
-   function Movie($image, $title, $description, $rating, $year) {
-      $this->image = $image;
-      $this->title = $title;
-      $this->description = $description;
-      $this->rating = $rating;
-      $this->year = $year;
-   }
-}
-
-// Get data from db
-require_once "dbConnect.php";
-
-// necessary to catch errors thrown from other files
-function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-   throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
-}
-set_error_handler("exception_error_handler");
-
-$db = getBD();
-
-// Just get some of the movies preparation
-$movieDB = $db->prepare("SELECT * FROM movie m INNER JOIN movie_group mg on m.movie_id = mg.movie_id AND mg.account_id = 2");
-$movieDB->execute();
-
-// Create array to hold all the users movies
-$movies = array();
-
-// Pass it into the object
-while($row = $moviesDB->fetch(PDO::FETCH_ASSOC)) {
-   $movie = new Movie($row["image"], $row["title"], $row["description"], $roe["rating"], $row["year"]);
-   array_push($movies, $movie);
-}
-
-// Set object in session
-$_SESSION["Movies"] = $movies;
-
-print_r($_SESSION);
-
-// Redirect to home.php
-header("Location: ../home.php");
-exit();
 
 ?>
