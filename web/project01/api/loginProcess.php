982ec70499e07725a5cfb3f0dbf5add734b09936
mcgaryp <portermcgary@me.com>
2020-02-07 16:01:41 -0700

    more updates to output

--- web/project01/api/loginProcess.php
+++ web/project01/api/loginProcess.php
@@ -1,6 +1,5 @@
 <?php
 
-   print("hello from process movies");
    session_start();
 
    $_SESSION["Account"];
@@ -25,22 +24,25 @@
    }
 
    // Get data from db
-   print("\nThis si at the top before the connect to DB");   
    require_once "dbConnect.php";
    
 
    // necessary to catch errors thrown from other files
-   // function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-   //    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
-   // }
+   function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+      throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+   }
 
-   // set_error_handler("exception_error_handler");
+   set_error_handler("exception_error_handler");
 
    $db = getBD();
+
    print("\nThis is right after db fun call");
+
    // Just get some of the movies preparation
    $movieDB = $db->prepare("SELECT * FROM movie m INNER JOIN movie_group mg on m.movie_id = mg.movie_id AND mg.account_id = 2");
    $movieDB->execute();
+
+   print("\nThis is right after db prepare and execute call");
 
    // Create array to hold all the users movies
    $movies = array();
