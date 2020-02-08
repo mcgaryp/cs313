7ce530ed65a5b7473c479c72333a399e7ba5c3af
mcgaryp <portermcgary@me.com>
2020-02-08 16:04:29 -0700

    style changes to card

--- web/project01/home.php
+++ web/project01/home.php
@@ -143,16 +143,16 @@
 
                      <!--First slide-->
                      <div class="carousel-item active">
-                     <div class="row">
+                     <div class="row justify-content-center">
                         <?php foreach ($movies as $m) { ?>
-                           <div class="col-md-4">
-                              <div class="card mb-2">
+                           <div class="col-md-auto">
+                              <div class="card">
                                  <img class="card-img-top" src="<?=$m->image?>"
                                     alt="<?=$m->title?> Movie Cover">
                                  <div class="card-body">
                                     <h4 class="card-title"><?=$m->title?></h4>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                    card's content.</p>
+                                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p> -->
                                  </div>
                               </div>
                            </div>
