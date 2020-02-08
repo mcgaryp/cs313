f1f88aa2ea40caad2e7f4623e8292cc36f1a7766
mcgaryp <portermcgary@me.com>
2020-02-08 15:10:28 -0700

    testing image

--- web/project01/home.php
+++ web/project01/home.php
@@ -146,13 +146,12 @@
                         <div class="row">
                            <div class="col-md-4">
                            <div class="card mb-2">
-                              <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
+                              <img class="card-img-top" src="<?=$movies[0]->image?>"
                                  alt="Card image cap">
                               <div class="card-body">
-                                 <h4 class="card-title"><?=$movies[0]->title?>>/h4>
+                                 <h4 class="card-title"><?=$movies[0]->title?></h4>
                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                                  card's content.</p>
-                                 <a class="btn btn-primary">Button</a>
                               </div>
                            </div>
                            </div>
