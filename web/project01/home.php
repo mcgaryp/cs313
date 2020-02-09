733df8defdfdae2758a3ebc190df8b6f8199b5a9
mcgaryp <portermcgary@me.com>
2020-02-09 02:04:59 -0700

    small change to footer of modal

--- web/project01/home.php
+++ web/project01/home.php
@@ -216,10 +216,10 @@
                                        <div class="modal-body">
                                        <?=$movies[$index]->description?>
                                        </div>
-                                       <div class="modal-footer">
+                                       <!-- <div class="modal-footer">
                                           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                           <button type="button" class="btn btn-primary">Save changes</button>
-                                       </div>
+                                       </div> -->
                                     </div>
                                  </div>
                               </div>
