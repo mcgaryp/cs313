1a734ea4c479b3198737f3dd3a0a985b824cc0e1
mcgaryp <portermcgary@me.com>
2020-02-11 16:43:48 -0700

    small adjsust

--- web/week06/team/insert.php
+++ web/week06/team/insert.php
@@ -17,7 +17,7 @@
       $statement->execute();
       // SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
       // get id of last inserted row - save in $userId
-      $userId = $db->lastInsertId("scripture_scripture_id_seq");
+      $scripture = $db->lastInsertId("scripture_scripture_id_seq");
    }
    catch (Exception $ex)
    {
