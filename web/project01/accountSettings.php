994b21b4e50428d3a07722e289b9900d59605bda
mcgaryp <portermcgary@me.com>
2020-02-10 22:54:29 -0700

    echo checking in account

--- web/project01/accountSettings.php
+++ web/project01/accountSettings.php
@@ -24,6 +24,7 @@
       <script src="https://kit.fontawesome.com/79ad9f74b9.js" crossorigin="anonymous"></script>
    </head>
    <body>
+      <?php print_r($_SESSION);?>
       <div>User account by the uername of: <?=$account->username?></div>
       <!-- Scripts -->
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
