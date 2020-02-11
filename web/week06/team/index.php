362849323821a27889e1b84155bc99434f5e2c01
mcgaryp <portermcgary@me.com>
2020-02-11 16:14:14 -0700

    saving team stuff

---
+++ web/week06/team/index.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<?php
+	require("dbConnect.php");
+	$db = get_db();
+?>
+<body>
+   <div class="container" style="margin-top:50px;">
+      <form action="insert.php" method="POST">
+         <div class="form-row">
+            <div class="col">
+               <input type="text" class="form-control" placeholder="Book" name="book">
+            </div>
+            <div class="col">
+               <input type="number" class="form-control" placeholder="Chapter" name="chapter">
+            </div>
+            <div class="col">
+               <input type="number" class="form-control" placeholder="Verse" name="verse">
+            </div>
+            <div class="col">
+               <!-- <textarea class="form-control" placeholder="Content" name content>
+                  <select id="inputFood" class="form-control" name="food">
+                     <?php
+                        // $statement = $db->prepare("SELECT * FROM w6_food");
+                        // $statement->execute();
+                        // while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                        // {
+                        //    $id   = $row['id'];
+                        //    $food = $row['food'];
+                        //    echo "<option value='$id'>$food</option>";
+                        // }
+                     ?>
+                  </select> -->
+            </div>
+            <div class="col">
+               <button class="btn btn-primary" type="submit">Save me some SCRIPTURE</button>
+            </div>
+         </div>
+      </form>
+   </div>
+</body>
---
+++ web/week06/team/run.sql
@@ -0,0 +1,41 @@
+DROP TABLE if EXISTS topic CASADE;
+DROP TABLE if EXISTS scripture CASCADE;
+DROP TABLE if EXISTS linker CASADE;
+
+
+CREATE TABLE topic (
+   topic_id    SERIAL      NOT NULL PRIMARY KEY,
+   topic_name  VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE scripture (
+   scripture_id   SERIAL   NOT NULL PRIMARY KEY,
+   book           TEXT     NOT NULL,
+   chapter        INTEGER  NOT NULL,
+   verse          INTEGER  NOT NULL,
+   content        TEXT     NOT NULL,
+);
+
+CREATE TABLE linker (
+   linker_id      SERIAL   NOT NULL PRIMARY KEY,
+   topic_id       INTEGER  NOT NULL REFERENCES topic(topic_id),
+   scripture_id   INTEGER  NOT NULL REFERENCES scripture(scripture_id)
+);
+
+INSERT INTO topic (
+   topic_name
+) VALUES (
+   'Faith'
+);
+
+INSERT INTO topic (
+   topic_name
+) VALUES (
+   'Sacrafice'
+);
+
+INSERT INTO topic (
+   topic_name
+) VALUES (
+   'Charity'
+);
