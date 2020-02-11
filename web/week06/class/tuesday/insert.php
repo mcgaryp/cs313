8f70f767972e6f66b275c1935e22a33fda91f383
mcgaryp <portermcgary@me.com>
2020-02-11 15:33:35 -0700

    Added somethings to the base

---
+++ web/week06/class/tuesday/insert.php
@@ -0,0 +1,31 @@
+<?php
+// retrieve POST data from the other page
+$first = $_POST["first"];
+$last = $_POST["last"];
+$food = $_POST["food"];
+
+require("dbConnect.php");
+$db = get_db();
+
+try
+{
+	// insert into database
+	$query = "INSERT INTO w6_user (first_name, last_name, food_type) VALUES (:first, :last, :food)";
+	$statement = $db->prepare($query);
+	$statement->bindParam(":first", $first);
+	$statement->bindParam(":last", $last);
+	$statement->bindParam(":food", $food);
+	$statement->execute();
+	
+	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
+	// get id of last inserted row - save in $userId
+}
+catch (Exception $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+header("Location: display.php/?personId=$userId", true);
+
+die(); 
+?>
