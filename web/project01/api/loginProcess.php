2f068bcfc02e3471ec3e3a0c2e1387e6be2ac0d2
mcgaryp <portermcgary@me.com>
2020-02-11 20:06:48 -0700

    try

--- web/project01/api/loginProcess.php
+++ web/project01/api/loginProcess.php
@@ -22,37 +22,41 @@
       $db = getBD();
       try {
          $query = "SELECT * FROM account a WHERE a.username = :user AND a.password = :pass;";
-      $accountTable = $db->prepare($query);
-      $accountTable->bindValue(':user', $user);
-      $accountTable->bindValue(':pass', $pass);
-      $accountTable->execute();
+         $accountTable = $db->prepare($query);
+         $accountTable->bindValue(':user', $user);
+         $accountTable->bindValue(':pass', $pass);
+         $accountTable->execute();
 
-      echo $query."<br>";
-      
-      // Check to see if we have the right account
-      if ($row = $accountTable->fetch(PDO::FETCH_ASSOC)) {
-         echo "Made it to this<br>";
-         $account = new Account($row['account_id'], $row['username'], $row['password'], $row['email']);
-      } else {
-         header("../index.php", true);
-      }
+         echo $query."<br>";
+         
+         // Check to see if we have the right account
+         if ($row = $accountTable->fetch(PDO::FETCH_ASSOC)) {
+            echo "Made it to this<br>";
+            $account = new Account($row['account_id'], $row['username'], $row['password'], $row['email']);
+         } else {
+            header("../index.php", true);
+         } 
 
-      echo " Setting up Querey<br>";
-      echo gettype($account->id)."<br>";
+         echo " Setting up Querey<br>";
+         echo gettype($account->id)."<br>";
 
-      // Get the account set up
-      $query = "SELECT nick_name FROM account a INNER JOIN user_profile up ON up.account_id = a.account_id AND a.account_id = $account->id;";
-      $profileTable = $db->prepare($query);
-      $profileTable->execute();
+         // Get the account set up
+         $query = "SELECT nick_name FROM account a INNER JOIN user_profile up ON up.account_id = a.account_id AND a.account_id = $account->id;";
+         $profileTable = $db->prepare($query);
+         $profileTable->execute();
 
-      echo $query;
+         echo $query;
 
-      $profiles = [];
+         $profiles = [];
 
-      // pull data from database
-      while($row = $profileTable->fetch(PDO::FETCH_ASSOC)) {
-         $profile = $row["nick_name"];
-         array_push($profiles, $profile);
+         // pull data from database
+         while($row = $profileTable->fetch(PDO::FETCH_ASSOC)) {
+            $profile = $row["nick_name"];
+            array_push($profiles, $profile);
+         }
+      } catch (Exception $e) {
+         echo "Error with DB. Details: $e";
+         die();
       }
 
       // set the session variables
