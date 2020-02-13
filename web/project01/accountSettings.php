0495f06bc6ed12e9a942f34e26506a0501f6f9c9
mcgaryp <portermcgary@me.com>
2020-02-12 17:10:28 -0700

    settings 2.0

--- web/project01/accountSettings.php
+++ web/project01/accountSettings.php
@@ -74,14 +74,59 @@ if (isset($_SESSION["account"])) {
       <div class="col">
          <h2>Settings</h2>
       </div>
+      <div class="col">
+         <div class="row">
+            <div class="col">
+               Parental Controls
+            </div>
+            <div class="col">
+               Manage Download Devices
+            </div>
+            <div class="col">
+               Add Movies
+            </div>
+            <div class="col">
+               Delet Movies
+            </div>
+            <div class="col">
+               Sign out
+            </div>
+         </div>
+      </div>
+   </div>
+   <hr>
+   <div class="row">
+      <div class="col">
+         <h2>My Profile</h2>
+      </div>
+      <div class="col">
+         <div class="row">
+            <div class="col">
+               Profile Name and Icon
+            </div>
+            <div class="col">
+               Manage Profiles
+            </div>
+         </div>
+         <div class="row">
+            <div class="col">
+               Subtitles
+            </div>
+            <div class="col">
+               Playback Settings
+            </div>
+            <div class="col">
+               Subtitle Appearance
+            </div>
+         </div>
+      </div>
    </div>
 
-   
-   <!-- Scripts -->
-   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   <script src="" async defer></script>
+      <!-- Scripts -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="" async defer></script>
 </body>
 
 </html>
