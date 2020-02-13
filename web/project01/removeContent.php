407c382f63c5849b3bc8f6aeb81cd0474c617ca4
mcgaryp <portermcgary@me.com>
2020-02-13 16:59:47 -0700

    something

--- web/project01/removeContent.php
+++ web/project01/removeContent.php
@@ -49,57 +49,76 @@ if (isset($_SESSION["account"])) {
       <form action="removeContent.php" method="POST" class="was-validated">
          <div class="row">
 
-               <!-- Title -->
-               <div class="col-md-4 mb-3">
-                  <input type="text" class="form-control is-valid" id="movieTitle" placeholder="Movie Title" name="title" required>
-                  <div class="invalid-feedback">
-                     What movie are we gonna remove from your library?
-                  </div>
+            <!-- Title -->
+            <div class="col-md-4 mb-3">
+               <input type="text" class="form-control is-valid" id="movieTitle" placeholder="Movie Title" name="title" required>
+               <div class="invalid-feedback">
+                  What movie are we gonna remove from your library?
                </div>
-
-               <!-- Button to confirm -->
-               <div class="col-md-4 mb-3">
-                  <button class="btn btn-warning" type="submit" name="delete">Delete</button>
-               </div>
-
             </div>
+
+            <!-- Button to confirm -->
+            <div class="col-md-4 mb-3">
+               <button class="btn btn-warning" type="submit" name="delete">Delete</button>
+            </div>
+
+         </div>
       </form>
-      <?php 
-         if (isset($delete)) {
-            if (isset($_POST["title"])) {
-               $title = $_POST["title"];
+      <?php
+      if (isset($delete)) {
+         if (isset($_POST["title"])) {
+            $title = $_POST["title"];
+         }
+
+         require "api/dbConnect.php";
+         $db = getBD();
+
+         // search database for title
+         try {
+            $query = 'SELECT * FROM movie m inner join movie_group mg on title = :title and mg.account_id = 2 and m.movie_id = mg.movie_id;';
+            $state = $db->prepare($query);
+            $state->bindValue(':title', $title);
+            // $state->bindValue(':id', $account->id);
+            $state->execute();
+
+            $movies = array();
+
+            while ($row = $state->fetch(PDO::FETCH_ASSOC)) {
+               $movie = new Movie($row["movie_id"], $row["image"], $row["title"], $row["description"], $row["rating"], $row["year"]);
+               array_push($movies, $movie);
             }
+         } catch (Exception $e) {
+            echo "Error with DB. Details: $e";
+            die;
+         } ?>
 
-            require "api/dbConnect.php";
-            $db = getBD();
-
-            // search database for title
-            try {
-               $query = 'SELECT * FROM movie m inner join movie_group mg on title = :title and mg.account_id = 2 and m.movie_id = mg.movie_id;';
-               $state = $db->prepare($query);
-               $state->bindValue(':title', $title);
-               // $state->bindValue(':id', $account->id);
-               $state->execute();
-
-               $movies = array();
-
-               while($row = $state->fetch(PDO::FETCH_ASSOC)) {
-                  $movie = new Movie($row["movie_id"], $row["image"], $row["title"], $row["description"], $row["rating"], $row["year"]);
-                  array_push($movies, $movie);
-               }
-            } catch (Exception $e) {
-               echo "Error with DB. Details: $e";
-               die;
-            } ?>
-
-            <table class="table table">
-         <?php foreach($movies as $movie) {
-               echo "i found this many movies!";
-            } ?>
-            </table>
+         <table class="table table-striped table-hover">
+            <thead class="thead-dark">
+               <th scope="col">Cover Image</th>
+               <th scope="col">Title</th>
+               <th scope="col">Description</th>
+               <th scope="col">Year Made</th>
+               <th scope="col">Rating</th>
+               <th scope="col"></th>
+            </thead>
+            <tbody>
+               <?php
+               foreach ($movies as $movie) { ?>
+                  <tr>
+                     <th scope="row"><?=$i?></th>
+                     <td><img src="<?=$movie->image?>" alt="<?=$movie->title?>" class="img-thumbnail"></td>
+                     <td><?=$movie->title?></td>
+                     <td><?=$movie->description?></td>
+                     <td><?=$movie->year?></td>
+                     <td><?=$movie->rating?></td>
+                     <td><button class="btn btn-danger"><i class="fas fa-trash"></i></button></td>
+                  </tr>
+               <?php } ?>
+            </tbody>
+         </table>
       <?php } ?>
       <!-- Show List of Movies that have that title with option to delete-->
-      
+
       <!-- Confirm that you want to delete it! -->
 
       <!-- delete movie -->
