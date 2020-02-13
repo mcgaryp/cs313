1e607e134388344ea706b0e30e141ffe71901bad
mcgaryp <portermcgary@me.com>
2020-02-12 17:04:33 -0700

    account settings start

--- web/project01/accountSettings.php
+++ web/project01/accountSettings.php
@@ -1,37 +1,87 @@
 <?php
 
-   include "classes/account.php";
+include "classes/account.php";
 
-   session_start();
+session_start();
 
-   $_SESSION["current"] = "account";
+$_SESSION["current"] = "account";
+$_SESSION["extension"] = "settings";
 
-   if (isset($_SESSION["account"])) {
-      $account = $_SESSION["account"];
-   }
+if (isset($_SESSION["account"])) {
+   $account = $_SESSION["account"];
+}
 
 ?>
 
 <!DOCTYPE html>
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title><?=$account->username?> Account</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link rel="stylesheet" href="css/home.css">
-      <script src="https://kit.fontawesome.com/79ad9f74b9.js" crossorigin="anonymous"></script>
-   </head>
-   <body>
-      <?php 
-         include "nav.php";
-      print_r($_SESSION);?>
-      <div>User account by the uername of: <?=$account->username?></div>
-      <!-- Scripts -->
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      <script src="" async defer></script>
-   </body>
+
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <title><?= $account->username ?> Account</title>
+   <meta name="description" content="">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <link rel="stylesheet" href="css/home.css">
+   <script src="https://kit.fontawesome.com/79ad9f74b9.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+   <?php
+   include "nav.php";
+   print_r($_SESSION); ?>
+   <div>User account by the uername of: <?= $account->username ?></div>
+   <h1>Account</h1>
+   <hr>
+   <div class="row">
+      <div class="col">
+         <h2>Membership & Billing</h2>
+      </div>
+      <div class="col">
+         <div class="row">
+            <div class="col">
+               Email
+            </div>
+            <div class="col">
+               Change email
+            </div>
+         </div>
+         <div class="row">
+            <div class="col">
+               Password: ********
+            </div>
+            <div class="col">
+               Change password
+            </div>
+         </div>
+         <div class="row">
+            <div class="col">
+               Phone: 479-696-1637
+            </div>
+            <div class="col">
+               Change phone number
+            </div>
+         </div>
+         <hr>
+         <div class="row">
+            <div class="col">
+               Billing
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col">
+         <h2>Settings</h2>
+      </div>
+   </div>
+
+   
+   <!-- Scripts -->
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   <script src="" async defer></script>
+</body>
+
 </html>
