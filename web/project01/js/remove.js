6823dc06f49022ff3e6544ba42f02b3894ae8c10
mcgaryp <portermcgary@me.com>
2020-02-13 19:32:08 -0700

    java scre

--- web/project01/js/remove.js
+++ web/project01/js/remove.js
@@ -1,17 +1,16 @@
 function callDelete(movieId) {
    var data = new FormData();
    data.append('movieId', movieId);
-   
+
    var xhttp = new XMLHttpRequest();
    xhttp.open('POST', 'api/delete.php', true);
    xhttp.onload = function () {
-         if (xhttp.response == 1)
-            window.location.assign("home.php");
-         else {
-            // Failed to delete
-            console.log("Failed to delete");
-            console.log(xhttp.response);
-         }
+      if (xhttp.response == 1)
+         window.location.assign("home.php");
+      else {
+         // Failed to delete
+         console.log("Failed to delete");
+         console.log(xhttp.response);
       }
    }
    xhttp.send(data);
