4a7bc525fa2c1c5e0218060d58892a888f78e8a1
mcgaryp <portermcgary@me.com>
2020-02-13 19:38:59 -0700

    lets try once more

--- web/project01/js/remove.js
+++ web/project01/js/remove.js
@@ -1,17 +1,21 @@
 function callDelete(movieId) {
+   console.log("made it to the ajax");
    var data = new FormData();
    data.append('movieId', movieId);
 
    var xhttp = new XMLHttpRequest();
    xhttp.open('POST', 'api/delete.php', true);
-   xhttp.onload = function () {
-      if (xhttp.response == 1)
-         window.location.assign("home.php");
-      else {
-         // Failed to delete
-         console.log("Failed to delete");
-         console.log(xhttp.response);
+   xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+         if (xhttp.response == 1)
+            window.location.assign("home.php");
+         else {
+            // Failed to delete
+            console.log("Failed to delete");
+            console.log(xhttp.response);
+         }
       }
    }
-   xhttp.send(data);
+   xhttp.send('movieId=' + movieId);
+   
 }
