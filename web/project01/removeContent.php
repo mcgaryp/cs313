d77f8b91c1bd60121ccb448a537c68b2dc791e61
mcgaryp <portermcgary@me.com>
2020-02-13 18:03:33 -0700

    styke

--- web/project01/removeContent.php
+++ web/project01/removeContent.php
@@ -136,13 +136,14 @@ if (isset($_SESSION["account"])) {
                               <div class="modal-dialog" role="document">
                                  <div class="modal-content">
                                     <div class="modal-header">
-                                       <h5 class="modal-title" id="<?= $movie->id ?>ModalLabel">Remove <?= $movie->title ?></h5>
                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                           <span aria-hidden="true">&times;</span>
                                        </button>
                                     </div>
                                     <div class="modal-body">
-                                       ...
+                                       <h5 class="modal-title" id="<?= $movie->id ?>ModalLabel">
+                                          Perminately DELETE <bold><?= $movie->title ?></bold> from your library
+                                       </h5>
                                     </div>
                                     <div class="modal-footer">
                                        <div class="row justify-content-center">
