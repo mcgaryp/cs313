23f7cdc647f06466bf50e7093b2161e84d934909
mcgaryp <portermcgary@me.com>
2020-02-13 18:38:49 -0700

    tyr once more

--- web/project01/js/remove.js
+++ web/project01/js/remove.js
@@ -3,7 +3,7 @@ function callDelete(movieId) {
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-         window.location.href = xhttp.responseText;
+         window.location.assign(xhttp.responseText);
       }
    }
    xhttp.open("POST", "api/delete.php?movieId=" + movieId, true);
