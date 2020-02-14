765001554283a9066d5cda787a75b5c7b4c42ade
mcgaryp <portermcgary@me.com>
2020-02-13 18:17:51 -0700

    testing ajax

--- web/project01/js/remove.js
+++ web/project01/js/remove.js
@@ -1,5 +1,7 @@
 function callDelete(movieId) {
+   console.log("made it to the ajax");
    var xhttp = new XMLHttpRequest();
    xhttp.open("POST", "api/delete.php?movieId=" + movieId, true);
    xhttp.send();
+   console.log("request complete");
 }
