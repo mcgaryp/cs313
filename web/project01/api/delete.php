dc562b0606e33d61a1f251b70f953cda3fe03069
mcgaryp <portermcgary@me.com>
2020-02-22 14:42:33 -0600

    try agaiun

--- web/project01/api/delete.php
+++ web/project01/api/delete.php
@@ -1,6 +1,7 @@
 <?php
 
 include "../classes/account.php";
+include "../classes/movie.php";
 
 session_start();
 
@@ -20,16 +21,28 @@ if (isset($_POST["movieId"])) {
 
    // delete from movie group relation first
    try {
+      $query = "SELECT * FROM movie WHERE movie_id = $movieId";
+      $state = $db->prepare($query);
+      $state->execute();
+
+      if($row = $state->fetch(PDO::FETCH_ASSOC)) {
+         $movie = new Movie($row["movie_id"], $row["image"], $row["title"], $row["description"], $row["rating"], $row["year"]);
+      }
+
       $query = "DELETE FROM movie WHERE movie_id = $movieId AND account_id = $account->id;";
       $state = $db->prepare($query);
       $state->execute();
-      header("location: ../");
+      
+      header("location: ../?success=true&title=$movie->title");
+
    } catch (Exception $e) {
       echo "Database trouble. Details: $e";
+      die;
    }
       
    // check to see if the movie group has that movie id in it if it doesnt then delete the movie
    // echo true;
 } else {
    echo "Movieid not set";
+   die;
 }
