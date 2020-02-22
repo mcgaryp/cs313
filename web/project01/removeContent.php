d94335ae1ce48b3c1fc480b63bccecd66399946d
mcgaryp <portermcgary@me.com>
2020-02-22 15:58:51 -0600

    toast stuff complete

--- web/project01/removeContent.php
+++ web/project01/removeContent.php
@@ -48,22 +48,28 @@ if (isset($_SESSION["account"])) {
 </head>
 
 <body>
-   <?php include "nav.php"; ?>
+   <?php include "nav.php";
 
-   <!-- Toast -->
-   <div class="toast" style="position: absolute; top: 60px; right: 0;">
-      <div class="toast-header">
-         <img src="..." class="rounded mr-2" alt="...">
-         <strong class="mr-auto">Bootstrap</strong>
-         <small>now</small>
-         <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-         </button>
+   if (isset($success)) { ?>
+
+      <!-- Toast -->
+      <div class="toast" style="position: absolute; top: 60px; right: 0;">
+         <div class="toast-header">
+            <!-- <img src="" class="rounded mr-2" alt="..."> -->
+            <strong class="mr-auto"><?php if ($success) {
+               echo "Successful"; } else { echo "Failure"; } ?></strong>
+            <small>now</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="toast-body">
+         <?php if ($success) {
+               echo "Successfully removed $title from your collection"; } else { echo "Failure to remove $title from your collection"; } ?>
+         </div>
       </div>
-      <div class="toast-body">
-         Hello, world! This is a toast message.
-      </div>
-   </div>
+
+   <?php } ?>
 
    <div class="container">
       <div class="row justify-content-center">
