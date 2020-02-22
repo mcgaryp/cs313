227c2ea7e78831aa78e77210f6e689e3d1082ce4
mcgaryp <portermcgary@me.com>
2020-02-22 15:36:53 -0600

    toasts

--- web/project01/removeContent.php
+++ web/project01/removeContent.php
@@ -50,6 +50,22 @@ if (isset($_SESSION["account"])) {
 <body>
    <?php include "nav.php"; ?>
 
+   <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+      <div class="toast" style="position: absolute; top: 0; right: 0;">
+         <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="...">
+            <strong class="mr-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="toast-body">
+            Hello, world! This is a toast message.
+         </div>
+      </div>
+   </div>
+
    <div class="container">
       <div class="row justify-content-center">
          <div class="col-auto">
@@ -152,7 +168,7 @@ if (isset($_SESSION["account"])) {
                                              <!-- delete movie -->
                                              <form method="POST" action="api/delete.php">
                                                 <input id="<?= $movie->id ?>" name="movieId" value="<?= $movie->id ?>" hidden>
-                                                <button type="submit" class="btn btn-danger" >Yes</button>
+                                                <button type="submit" class="btn btn-danger">Yes</button>
                                              </form>
                                           </div>
                                        </div>
@@ -176,7 +192,7 @@ if (isset($_SESSION["account"])) {
       } ?>
 
    </div>
-   
+
 
    </div>
 
