db5318e0683d10403d93abe21c2ff7cf8ba5812e
mcgaryp <portermcgary@me.com>
2020-02-22 12:27:04 -0600

    trying somtehitn

--- SQLScripts/query.sql
+++ SQLScripts/query.sql
@@ -1,11 +1,11 @@
 select * from common_lookup;
-select movie_id, title, rating from movie;
+select movie_id,account_id, title, rating from movie;
 select * from account;
 -- select * from movie_group;
 select * from user_profile;
 
 -- Selects the movies to display on the home page
-select title, rating, year from movie where account_id = 2;
+select title, rating, year, account_id from movie where account_id = 2;
 
 -- selects the profiles
 select up.nick_name from account a inner join user_profile up on up.account_id = a.account_id and a.account_id = 2;
@@ -17,10 +17,10 @@ SELECT * FROM account a WHERE a.username = 'porter' AND a.password = 'password';
 SELECT * FROM common_lookup WHERE context = 'RATING';
 
 -- selects the movies based on movie id
-SELECT title, rating, year FROM movie WHERE movie_id = 5;
+SELECT title, rating, year, account_id FROM movie WHERE movie_id = 5;
 
 -- Deletes movie based on id and account
-DELETE FROM movie WHERE movie_id = 5 AND account_id = 1;
+DELETE FROM movie WHERE movie_id = 8 AND account_id = 1;
 
 -- Searches for specific movies in an account
-SELECT movie_id, account_id, title, rating, year FROM movie WHERE title ILIKE 'lo%' AND account_id = 1;
+SELECT movie_id, account_id, title, rating, year FROM movie WHERE title ILIKE '%or%' AND account_id = 2;
