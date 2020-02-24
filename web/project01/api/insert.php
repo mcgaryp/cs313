528027e716348018e75d18adb0f827c46acaf4af
mcgaryp <portermcgary@me.com>
2020-02-24 00:49:16 -0700

    try to create account

--- web/project01/api/insert.php
+++ web/project01/api/insert.php
@@ -56,20 +56,22 @@ if (isset($_POST["create"])) {
       // get the data
       if ($row = $state->fetch(PDO::FETCH_ASSOC)) {
          $account = new Account($row['account_id'], $row['username'], $row['password'], $row['email']);
+
+         // Create account and add to session
+         $_SESSION["account"] = $account;
+
+         // TODO Send to profile pagez
+         header("location: ../profiles.php", true);
+
       } else {
          // if failed to find account
          header("location: ../index.php?error=Incorrect Username or Password", true);
       }
+      
    } catch (Exception $e) {
       echo "Error with DB. Details: $e";
       die;
    }
-
-   // Create account and add to session
-   $_SESSION["account"] = $account;
-
-   // TODO Send to profile pagez
-   header("location: ../profiles.php", true);
 }
 
 // Are we adding movies?
@@ -126,7 +128,6 @@ if (isset($_POST["add"])) {
       header("location: ../addContent.php?success=0&error=$e&title=$title", true);
       die;
    }
-
 }
 
 // are we adding a profile?
@@ -161,7 +162,7 @@ if (isset($_POST["addProfile"])) {
       $state = $db->prepare($query);
       $state->bindValue(':id', $account->id);
       $state->bindValue(':icon', $icon);
-      $state->bindValue(':nick',$nickname);
+      $state->bindValue(':nick', $nickname);
 
       // execute
       $state->execute();
@@ -178,7 +179,6 @@ if (isset($_POST["addProfile"])) {
       // send back to profiles to choose your new profile
       header("location: ../profiles.php", true);
       die;
-
    } catch (Exception $e) {
       header("location: ../createProfile.php?success=0&error=$e", true);
       die;
