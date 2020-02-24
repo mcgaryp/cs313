6b8ff8e04d358cb160aada080070afe2d60bde35
mcgaryp <portermcgary@me.com>
2020-02-24 01:15:36 -0700

    style

--- web/project01/createProfile.php
+++ web/project01/createProfile.php
@@ -67,70 +67,74 @@ if (isset($success)) { ?>
 <?php } ?>
 
 <div class="container">
-   <form action="api/insert.php" method="POST" class="was-validated">
-      <div class="row">
-         <!-- nickname -->
-         <div class="col-md-4 mb-3 form-group">
-            <label for="profileNickname">Profile Nickname</label>
-            <input type="text" class="form-control is-valid" id="profileNickname" placeholder="Darth Vader" name="nickname" required>
-            <div class="invalid-feedback">
-               Don't be evil now and pick a good name!
+   <div class="row justify-content-center">
+      <div class="col-auto">
+         <form action="api/insert.php" method="POST" class="was-validated">
+            <div class="row">
+               <!-- nickname -->
+               <div class="col-md-4 mb-3 form-group">
+                  <label for="profileNickname">Profile Nickname</label>
+                  <input type="text" class="form-control is-valid" id="profileNickname" placeholder="Darth Vader" name="nickname" required>
+                  <div class="invalid-feedback">
+                     Don't be evil now and pick a good name!
+                  </div>
+               </div>
+
             </div>
-         </div>
+            <div class="row">
+               <!-- have an option to provide icon of there own!? -->
+               <!-- place option toggle here -->
 
-      </div>
-      <div class="row">
-         <!-- have an option to provide icon of there own!? -->
-         <!-- place option toggle here -->
+               <!-- Icon -->
+               <div class="col-md-4 mb-3 form-group">
+                  <label for="profileIcon">Choose and Icon!</label>
+                  <select class="custom-select is-valid" id="profileIcon" name="icon" required>
+                     <option value="">Select Icon</option>
+                     <!-- add icons from the table generated! -->
+                     <?php
 
-         <!-- Icon -->
-         <div class="col-md-4 mb-3 form-group">
-            <label for="profileIcon">Choose and Icon!</label>
-            <select class="custom-select is-valid" id="profileIcon" name="icon" required>
-               <option value="">Select Icon</option>
-               <!-- add icons from the table generated! -->
-               <?php
+                     require "api/dbConnect.php";
+                     $db = getBD();
 
-               require "api/dbConnect.php";
-               $db = getBD();
+                     // create my query
+                     $query = "SELECT * FROM common_lookup WHERE context = 'ICON';";
+                     $state = $db->prepare($query);
 
-               // create my query
-               $query = "SELECT * FROM common_lookup WHERE context = 'ICON';";
-               $state = $db->prepare($query);
+                     // run query
+                     $state->execute();
 
-               // run query
-               $state->execute();
+                     // get the values
+                     try {
+                        while ($row = $state->fetch(PDO::FETCH_ASSOC)) {
+                           $icon = $row["meaning"];
+                           $name = $row["type"];
+                           echo "<option value='$icon'>$name</option>";
+                        }
+                     } catch (Exception $e) {
+                        echo "Error with DB. Details: $e";
+                        die;
+                     }
 
-               // get the values
-               try {
-                  while ($row = $state->fetch(PDO::FETCH_ASSOC)) {
-                     $icon = $row["meaning"];
-                     $name = $row["type"];
-                     echo "<option value='$icon'>$name</option>";
-                  }
-               } catch (Exception $e) {
-                  echo "Error with DB. Details: $e";
-                  die;
-               }
+                     ?>
+                  </select>
+                  <div class="invalid-feedback">Choose a cool Icon!</div>
+               </div>
 
-               ?>
-            </select>
-            <div class="invalid-feedback">Choose a cool Icon!</div>
-         </div>
+               <!-- option to upload icon here if toggle pushed -->
 
-         <!-- option to upload icon here if toggle pushed -->
-
-      </div>
-      <div class="row">
-         <!-- Submit button -->
-         <div class="row justtify-content-center">
-            <div class="col-auto">
-               <button class="btn btn-success" type="submit" name="addProfile">Create Profile</button>
             </div>
-         </div>
+            <div class="row">
+               <!-- Submit button -->
+               <div class="row justtify-content-center">
+                  <div class="col-auto">
+                     <button class="btn btn-success" type="submit" name="addProfile">Create Profile</button>
+                  </div>
+               </div>
 
+            </div>
+         </form>
       </div>
-   </form>
+   </div>
 </div>
 
 <!-- Scripts -->
